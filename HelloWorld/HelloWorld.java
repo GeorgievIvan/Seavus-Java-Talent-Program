a93f7ed7833f17a6fb47ead3d77b4ed447857fcb
“Ivan” <ivangeo12@hotmail.com>
2016-04-21 23:07:56 +0200

    HelloWorld added

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+/**
+* The HelloWorldAppclass implements an application
+* that simply prints "Hello Java Developers!" message to standard output.
+*
+* @paramargs
+*/
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java Developers!");
+	}
+}
